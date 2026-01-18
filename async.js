f187def06ff3c2a75e1acafa80c0ea24b52cb094
sohanur1005 <sohanur.sr1005@gmail.com>
2026-01-18 22:45:44 +0600

    send this

---
+++ async.js
@@ -0,0 +1,15 @@
+
+
+const myLoader=() =>{
+    return new Promise((resolve,reject) =>{
+        const success=Math.random();
+        if(success <=0.5)
+        {
+            resolve(success);
+
+        }
+        else{
+            reject(success)
+        }
+    })
+}
